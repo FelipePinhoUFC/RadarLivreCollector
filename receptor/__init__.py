1fb920a578d0302d6349f46a2e9fd1ce2ccf6e35
Felipe <felipe76857685@gmail.com>
2016-08-14 00:17:16 -0300

    updating

--- receptor/__init__.py
+++ receptor/__init__.py
@@ -1,5 +1,7 @@
 import logging as log
 
+from pyModeS import adsb
+
 __MAP_BUFFER = {}
 
 class RawData():
@@ -12,6 +14,32 @@ class RawData():
         self.timestamp = raw["timestamp"]["integer"]
         self.downlinkformat = raw["downlinkformat"]
         self.frame = raw["frame"]
+
+    def __cmp__(self, other):
+        return self.timestamp.__cmp__(other.timestamp)
+
+    def __repr__(self):
+        return "RawData: [tt=%d, dl=%d, fm=%s]" % self.timestamp, self.downlinkformat, self.frame
+
+
+class MessageBuffer():
+
+    dataId = None
+    dataPositionEven = None
+    dataPositionOdd = None
+    dataVelocity = None
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.iteritems():
+            setattr(self, k, v)
+
+    def addRawData(self, rawData):
+        type = adsb.typecode(rawData.frame[1:29])
+        if type >= 1 and type <= 4:
+            pass
+
+    def isComplete(self):
+        return self.dataId and self.dataPositionEven and self.dataPositionOdd and self.dataVelocity
 
 
 def __onOpen(err):
@@ -31,3 +59,11 @@ def __onClose(err):
 def __onMessage(data):
     if data:
         rawData = RawData(data)
+
+        if rawData.downlinkformat == 17:
+            icao24 = adsb.icao(rawData.frame[1:29])
+
+            if not icao24 in __MAP_BUFFER:
+                __MAP_BUFFER[icao24] = []
+
+            __MAP_BUFFER[icao24].append(rawData)
