47320ba5e0d00b9598d923cf212dba0e0404db31
felipe <felipe76857685@gmail.com>
2016-08-27 15:11:09 -0300

    Removing .pyc

--- config.py
+++ config.py
@@ -1,29 +1,27 @@
-SERVER_HOST = "200.129.38.153"
+from receptor import rootConfig
 
-LOGIN = "root"
-PASSWORD = "root7685"
-COLLECTOR_ID = "b8599899-093f-4c59-a36b-4cb23476bf2d"
+rootConfig.SERVER_HOST = "200.129.38.153"
 
-# Override with atual colector position
-LATITUDE = -4.864177
-LONGITUDE = -39.581899
+rootConfig.LOGIN = "root"
+rootConfig.PASSWORD = "root7685"
+rootConfig.COLLECTOR_ID = "b8599899-093f-4c59-a36b-4cb23476bf2d"
 
 # Define a location to local database
-DATABASE_DIR = "/home/felipepinho/SistemaV4/RadarLivreCollector/data"
-DATABASE_DIR = "/home/felipe/Projects/WEB/RadarLivre/RadarLivreCollector/data"
+rootConfig.DATABASE_DIR = "/home/felipepinho/SistemaV4/RadarLivreCollector/data"
+rootConfig.DATABASE_DIR = "/home/felipe/Projects/WEB/RadarLivre/RadarLivreCollector/data"
 
 # Define a location to log files
-LOG_DIR = "/home/felipepinho/SistemaV4/RadarLivreCollector/log"
-LOG_DIR = "/home/felipe/Projects/WEB/RadarLivre/RadarLivreCollector/log"
+rootConfig.LOG_DIR = "/home/felipepinho/SistemaV4/RadarLivreCollector/log"
+rootConfig.LOG_DIR = "/home/felipe/Projects/WEB/RadarLivre/RadarLivreCollector/log"
 
 # Override with the collector adress, normaly located in /dev/ttyACM...
-COLLECTOR_ADDRESS = '/dev/ttyACM0'
+rootConfig.COLLECTOR_ADDRESS = '/dev/ttyACM0'
 
 # 10 seconds. After this time, the not sent messages will be deleteds
-MAX_MESSAGE_AGE = 60 * 1000
+rootConfig.MAX_MESSAGE_AGE = 60 * 1000
 
-DATA_OUTPUT_ENABLED = True
-DATA_OUTPUT_HOST = "127.0.0.1"
-DATA_OUTPUT_PORT = 30003
+rootConfig.DATA_OUTPUT_ENABLED = True
+rootConfig.DATA_OUTPUT_HOST = "127.0.0.1"
+rootConfig.DATA_OUTPUT_PORT = 30003
 
-LOCAL_DATA_ENABLED = False
+rootConfig.LOCAL_DATA_ENABLED = False
